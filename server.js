6dee7998795472427b99f50bb6ef0df21369a862
Miguel Sánchez Torrijo <32265599+miguelst1@users.noreply.github.com>
2019-03-13 11:03:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,27 @@ const path = require('path');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const routes = require('./routes');
+const config = require('./config');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/hospital', { useNewUrlParser: true })
+mongoose.connect(config.db_uri, { useNewUrlParser: true })
 .then(db   => console.log ('Conexión correcta a la BD'))
 .catch(err => console.log ('Error en la conexión a la BD'));
+
+// --- MIDDLEWARE
+// Para redirigir trafico HTTP a HTTPS
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    else
+      next();
+  });
 
 app.use(express.static(path.join(__dirname , 'public')));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api', routes);
 
-app.listen (3000, 
+app.listen (config.port, 
             () => console.log('Servidor iniciado en puerto ${config.port}'));
